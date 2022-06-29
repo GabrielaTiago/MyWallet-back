ce3e6259dbd4b2eb9dce3c20c66b2ddc0328108d
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-06-29 00:34:12 -0300

    Feat: Created the validation of the values email and password

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import { MongoClient } from "mongodb";
+import joi from "joi";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
@@ -11,21 +12,30 @@ server.use(cors());
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
-mongoClient.connect(() => {
-    db = mongoClient.db(process.env.DATABASE)
+mongoClient.connect().then(() => {
+    db = mongoClient.db(process.env.MONGO_DATABASE_NAME);
 });
 
 
+const singInSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required().min(4)
+});
 
-server.post("/sing-in", async (require, response) => {
+
+server.post("/sign-in", async (require, response) => {
     const userSingIn = require.body;
+
+    const validation = singInSchema.validate(userSingIn, { abortEarly: true });
+
+    if(validation.error){
+        response.status(422).send(validation.error.details);
+    }
 
     try {
         await db
             .collection("users")
-            .insertOne({
-                name: name,
-            });
+            .insertOne({userSingIn});
     } catch (error) {
         console.error(error);
         response.status(500).send("Erro na requisição");
