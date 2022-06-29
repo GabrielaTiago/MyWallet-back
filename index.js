dfce81b62ab3ea44b3405a2b5940237cd7ea1d4d
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-06-28 22:23:14 -0300

    Feat: Creating the project's .env enviroment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
-
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const server = express();
 
-server.listen(5000);
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+server.listen(process.env.PORT, () => {
+    console.log(`O servidor está funcionando na porta ${process.env.PORT}`)
+});
