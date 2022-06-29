984df72b5a57b74bd68b06c80db616f90cc902d8
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-06-28 22:58:17 -0300

    Feat: Created the inicial steps to the route sign-in

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 import express, { json } from "express";
 import { MongoClient } from "mongodb";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 const server = express();
+server.use(json());
+server.use(cors());
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 mongoClient.connect(() => {
     db = mongoClient.db(process.env.DATABASE)
+});
+
+
+
+server.post("/sing-in", async (require, response) => {
+    const userSingIn = require.body;
+
+    try {
+        await db
+            .collection("users")
+            .insertOne({
+                name: name,
+            });
+    } catch (error) {
+        console.error(error);
+        response.status(500).send("Erro na requisição");
+    }
 });
 
 server.listen(process.env.PORT, () => {
