9ca5eda7e1726fda2faec7ac4988b2a9292d06d8
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-07 19:13:33 -0300

    Feat: Implements the token validation middleware

--- src/router/walletRouter.js
+++ src/router/walletRouter.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { getWallet, postTransaction } from "../controllers/walletController.js";
-import validateToken from "../middlewares/tokenMiddleware.js";
 import schemasValidation from "../middlewares/schemasValidation.js";
+import tokenValidation from "../middlewares/tokenValidation.js";
 
 const walletRouter = Router();
 
-walletRouter.get("/transactions", validateToken, getWallet);
+walletRouter.use(tokenValidation);
+walletRouter.get("/transactions", getWallet);
 walletRouter.post(
   "/transactions",
-  validateToken,
   schemasValidation("transaction"),
   postTransaction
 );
