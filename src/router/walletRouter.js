d07a4c8be70b6e41bd55cc19588a06fcd1e67170
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-07 18:53:05 -0300

    Feat: Implements the schemas validation middleware

--- src/router/walletRouter.js
+++ src/router/walletRouter.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { getWallet, postTransaction } from "../controllers/walletController.js";
 import validateToken from "../middlewares/tokenMiddleware.js";
-import transactionsMiddleware from "../middlewares/transactionsMiddleware.js";
+import schemasValidation from "../middlewares/schemasValidation.js";
 
 const walletRouter = Router();
 
 walletRouter.get("/transactions", validateToken, getWallet);
-walletRouter.post("/transactions", validateToken, transactionsMiddleware, postTransaction);
+walletRouter.post(
+  "/transactions",
+  validateToken,
+  schemasValidation("transaction"),
+  postTransaction
+);
 
 export default walletRouter;
