ed43344ab37719f04a77442400d5364b2120d381
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-07 12:48:34 -0300

    Feat: Creates the error handler middleware

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
+import "express-async-errors";
 import express, { json } from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import authRouter from "./routes/authRoutes.js";
-import walletRoutes from "./routes/walletRoutes.js"
-dotenv.config();
+import { config } from "dotenv";
+import authRouter from "./router/authRouter.js";
+import walletRoutes from "./router/walletRouter.js";
+import errorHandler from "./middlewares/errorHandler.js";
+config();
 
 const server = express();
 server.use(json(), cors());
@@ -11,6 +13,8 @@ server.use(json(), cors());
 server.use(authRouter);
 server.use(walletRoutes);
 
+server.use(errorHandler);
+
 server.listen(process.env.PORT, () => {
-    console.log(`The server is running on port ${process.env.PORT}`);
+  console.log(`The server is running on port ${process.env.PORT}`);
 });
