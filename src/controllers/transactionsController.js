bf0bd25d0aae12a9af0d30382c5f7234dbe74e94
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-08 17:41:54 -0300

    Feat: Creates the controller function to handle the update transactions

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -1,9 +1,7 @@
 import transactionsService from "../services/transactionsService.js";
 
 async function getAllTransactions(req, res) {
-  const { id: userId } = res.locals.user;
-
-  const transactions = await transactionsService.getAllTransactions(userId);
+  const transactions = await transactionsService.getAllTransactions();
 
   res.status(200).send(transactions);
 }
@@ -12,14 +10,36 @@ async function createTransaction(req, res) {
   const { id: userId } = res.locals.user;
   const { amount, description, type } = req.body;
 
-  await transactionsService.createTransaction(userId, amount, description, type);
+  await transactionsService.createTransaction(
+    userId,
+    amount,
+    description,
+    type
+  );
 
   res.sendStatus(201);
 }
 
+async function updateTransaction(req, res) {
+  const { amount, description, type } = req.body;
+  const { id: userId } = res.locals.user;
+  const { id } = req.params;
+
+  await transactionsService.updateTransaction(
+    userId,
+    id,
+    amount,
+    description,
+    type
+  );
+
+  res.status(200).send("Updated!");
+}
+
 const transactionsController = {
-  getAllTransactions,
   createTransaction,
+  getAllTransactions,
+  updateTransaction,
 };
 
 export default transactionsController;
