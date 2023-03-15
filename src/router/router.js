9734389f56ac429a4aadc160abe56c4438c019b9
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-15 16:41:58 -0300

    Docs: Creates the API documentation with swagger

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "../../swagger.json" assert { type: "json" };
 import authRouter from "./authRouter.js";
 import transactionsRouter from "./transactionsRouter.js";
 
 const router = Router();
 
+router.use("/documentation", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 router.use(authRouter);
 router.use(transactionsRouter);
 
