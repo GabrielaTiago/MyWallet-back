f2894d1784cd53a13de5b8f0dc5f81c3317b4449
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-08-20 22:41:52 -0300

    Fix: Database access

--- src/repositories/authRepository.js
+++ src/repositories/authRepository.js
@@ -1,26 +1,24 @@
-import { ObjectId } from "mongodb";
-import { database } from "../database/mongodb.js";
+import { ObjectId } from 'mongodb';
+import { database } from '../database/mongodb.js';
 
 async function findUserByEmail(email) {
-  const user = await database.collection("users").findOne({ email });
-  return user;
+	const user = await database.collection('users').findOne({ email });
+	return user;
 }
 
 async function createUser(name, email, password) {
-  await database.collection("users").insertOne({ name, email, password });
+	await database.collection('users').insertOne({ name, email, password });
 }
 
 async function findUserById(id) {
-  const user = await database
-    .collection("users")
-    .findOne({ _id: ObjectId(id) });
-  return user;
+	const user = await database.collection('users').findOne({ _id: new ObjectId(String(id)) });
+	return user;
 }
 
 const authRepository = {
-  createUser,
-  findUserByEmail,
-  findUserById,
+	createUser,
+	findUserByEmail,
+	findUserById,
 };
 
 export default authRepository;
--- src/repositories/transactionsRepository.js
+++ src/repositories/transactionsRepository.js
@@ -1,73 +1,66 @@
-import { ObjectId } from "mongodb";
-import { database } from "../database/mongodb.js";
+import { ObjectId } from 'mongodb';
+import { database } from '../database/mongodb.js';
 
 async function getAllTransactions(userId) {
-  const transactions = await database
-    .collection("transactions")
-    .find({ userId })
-    .toArray();
-  return transactions;
+	const transactions = await database.collection('transactions').find({ userId }).toArray();
+	return transactions;
 }
 
 async function getBalance(userId) {
-  const total = await database
-    .collection("transactions")
-    .aggregate([
-      {
-        $match: { userId },
-      },
-      {
-        $group: {
-          _id: null,
-          balance: { $sum: "$amount" },
-        },
-      },
-    ])
-    .toArray();
-  return total[0].balance;
+	const total = await database
+		.collection('transactions')
+		.aggregate([
+			{
+				$match: { userId },
+			},
+			{
+				$group: {
+					_id: null,
+					balance: { $sum: '$amount' },
+				},
+			},
+		])
+		.toArray();
+	return total[0].balance;
 }
 
 async function createTransaction(userId, day, amount, description, type) {
-  await database
-    .collection("transactions")
-    .insertOne({ userId, day, amount, description, type });
+	await database.collection('transactions').insertOne({ userId, day, amount, description, type });
 }
 
 async function updateTransaction(transactionId, amount, description, type) {
-  await database.collection("transactions").updateOne(
-    {
-      _id: ObjectId(transactionId),
-    },
-    {
-      $set: {
-        amount,
-        description,
-        type,
-      },
-    }
-  );
+	await database.collection('transactions').updateOne(
+		{
+			_id: new ObjectId(String(transactionId)),
+		},
+		{
+			$set: {
+				amount,
+				description,
+				type,
+			},
+		}
+	);
 }
 
 async function deleteTransaction(transactionId) {
-  await database.collection("transactions").deleteOne({
-    _id: ObjectId(transactionId),
-  });
+	await database.collection('transactions').deleteOne({
+		_id: new ObjectId(String(transactionId)),
+	});
 }
 
 async function findTransactionById(transactionId) {
-  const transaction = await database
-    .collection("transactions")
-    .findOne({ _id: ObjectId(transactionId) });
-  return transaction;
+	const transaction = await database.collection('transactions').findOne({ _id: new ObjectId(String(transactionId)) });
+	return transaction;
 }
 
 const transactionsRepository = {
-  createTransaction,
-  deleteTransaction,
-  findTransactionById,
-  getAllTransactions,
-  getBalance,
-  updateTransaction,
+	createTransaction,
+	deleteTransaction,
+	findTransactionById,
+	getAllTransactions,
+	getBalance,
+	updateTransaction,
 };
 
 export default transactionsRepository;
